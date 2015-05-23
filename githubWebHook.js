05aea90563f09db63e22ed7600078c4f15c54bf2
Aravind Pedapudi <aravindp1510@gmail.com>
2015-05-23 17:04:57 +0530

    Proper error handling and header verification

--- githubWebHook.js
+++ githubWebHook.js
@@ -4,13 +4,40 @@ var crypto = require('crypto');
 
 function webhookInit(app, githubSecret) {
     app.post('/repoPush', function (req, res) {
+        verifyHMAC(req, function (error, statusCode) {
+            if (error) {
+                console.error('Couldn\'t verify HMAC', error);
+            }
+            res.sendStatus(statusCode);
+        });
+    });
+}
+
+function verifyHMAC(req, returnStatus) {
+    var signatureHeader = req.get('X-Hub-Signature') || '';
+    var signature = signatureHeader.split('=')[1]; 
+    var statusCode = 403;
+    var error = {
+        message: ''
+    };
+    if (typeof signature !== 'undefined') {
+        console.log('Signature received: ', signature);
         var hmac = crypto.createHmac('sha1', githubSecret);
         req.on('data', function (d) {
             hmac.update(d);
         });
         req.on('end', function () {
-            console.log('hmac: ', hmac.digest('hex'));
+            var digest = hmac.digest('hex');
+            console.log('HMAC generated: ', digest);
+            if (digest === signature) {
+                statusCode = 204;
+                return returnStatus(null, statusCode);
+            }
+            error.message = 'Signature mismatch';
+            return returnStatus(error, statusCode);
         });
-        res.sendStatus(201);
-    });
+    } else {
+        error.message = 'No header available';
+        return returnStatus(error, statusCode);
+    }
 }
