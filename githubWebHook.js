e4f054d0e3736bfaf8f256df66749f7ae34b7c42
Aravind Pedapudi <aravindp1510@gmail.com>
2015-05-23 18:53:31 +0530

    Removed body parser in favor of JSON.parse-ing raw data, as crypto functions need the raw form

--- githubWebHook.js
+++ githubWebHook.js
@@ -2,16 +2,19 @@ module.exports = webhookInit;
 
 var crypto = require('crypto');
 var exec = require('child_process').exec;
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 function webhookInit(app, githubSecret) {
-    app.use('/repoPush', bodyParser.json());
+    //app.use('/repoPush', bodyParser.json());
     app.post('/repoPush', function (req, res) {
         verifyHMAC(req, githubSecret, function (error, statusCode) {
             if (error) {
                 console.error('Couldn\'t verify HMAC', error);
-            } else if (req.body.hasOwnProperty('ref') && app.get('current git ref') === req.body.ref) { 
-                pullGithub(ref);
+            } else {
+                req.body = JSON.parse(req.body);
+                if (req.body.hasOwnProperty('ref') && app.get('current git ref') === req.body.ref) { 
+                    pullGithub(ref);
+                }
             }
             res.sendStatus(statusCode);
         });
@@ -28,10 +31,13 @@ function verifyHMAC(req, githubSecret, returnStatus) {
     if (typeof signature !== 'undefined') {
         console.log('Signature received: ', signature);
         var hmac = crypto.createHmac('sha1', githubSecret);
+        var data = "";
         req.on('data', function (d) {
+            data += d.toString('utf-8');
             hmac.update(d);
         });
         req.on('end', function () {
+            req.body = data;
             var digest = hmac.digest('hex');
             console.log('HMAC generated: ', digest);
             if (digest === signature) {
